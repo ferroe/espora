db71615cb8a08baee079b969a75c8c729122dc96
Jannlinda <jann.alvarez@ciencias.unam.mx>
2025-09-10 00:18:11 -0600

    feat(sabores): Implementación de sabores de helado

---
+++ practica02/Practica02_espora/src/main/java/mx/unam/fciencias/espora/practica2/Chocolate.java
@@ -0,0 +1,24 @@
+package mx.unam.fciencias.espora.practica2;
+
+/**
+ * Esta es una clase que representa al sabor Chocolate de un helado.
+ * La cual extiende de la clase abstracta Helado.
+ */
+public class HeladoChocolate extends Helado {
+
+    /**
+     * Método constructor que inicializa a la descripción del helado sabor Chocolate.
+     */
+    public HeladoChocolate() {
+        descripcion = "Helado sabor Chocolate";
+    }
+
+    /**
+     * Método que devuelve el costo del helado sabor Chocolate.
+     * @return el costo del helado sabor Chocolate.
+     */
+    @Override
+    public double getCosto() {
+        return 50.0;
+    }
+}
---
+++ practica02/Practica02_espora/src/main/java/mx/unam/fciencias/espora/practica2/Vainilla.java
@@ -0,0 +1,24 @@
+package mx.unam.fciencias.espora.practica2;
+
+/**
+ * Esta es una clase que representa al sabor Vainilla de un helado.
+ * La cual extiende de la clase abstracta Helado.
+ */
+public class HeladoVainilla extends Helado {
+
+    /**
+     * Método constructor que inicializa a la descripción del helado sabor Vainilla.
+     */
+    public HeladoVainilla() {
+        descripcion = "Helado sabor Vainilla";
+    }
+
+    /**
+     * Método que devuelve el costo del helado sabor Vainilla.
+     * @return el costo del helado sabor Vainilla.
+     */
+    @Override
+    public double getCosto() {
+        return 50.0;
+    }
+}
