c11e4e1f91b64da7733fd1f1088cb8199117c640
Ricardo Buendia <ricardobuendia@ciencias.unam.mx>
2025-09-22 23:59:20 -0600

    feat(campos): Agregar campos de entrenamiento

---
+++ practica03/Practica03_espora/src/main/java/mx/unam/fciencias/espora/practica3/MontanaEspiritual.java
@@ -0,0 +1,22 @@
+package mx.unam.fciencias.espora.practica3;
+
+/**
+ * Clase para MontanaEspiritual
+ * 
+ * Clase para el lugar Montaña Espiritual donde se entrenan a los
+ * mejores Ninjas que hay.
+ * 
+ * @author Equipo Espora
+ * @version 1.0
+ */
+
+public class MontanaEspiritual extends CampoEntrenamiento {
+
+    /**
+     * Método constructor con su descripcion de la Montana Espiritual
+     */
+    public MontanaEspiritual(){
+        super("La Montana Espiritual, donde solo los mejores han" +
+                    "tocado el lugar, simbolo de excelencia para los Ninjas");
+    }
+}   
---
+++ practica03/Practica03_espora/src/main/java/mx/unam/fciencias/espora/practica3/ValleDelDragon.java
@@ -0,0 +1,22 @@
+package mx.unam.fciencias.espora.practica3;
+
+/**
+ * Clase para ValleDelDragon
+ * 
+ * Clase para el lugar de entrenamiento Valle del Dragon
+ * donde entrenan a los Ninjas que van iniciando.
+ * 
+ * @author Equipo Espora
+ * @version 1.0
+ */
+
+public class ValleDelDragon extends CampoEntrenamiento { 
+
+    /**
+     * Método constructor con su descripcion del Valle del Dragon
+     */
+    public ValleDelDragon(){
+        super("El Valle del Dragon, lugar donde se inicia el " +
+                    "viaje para ocnvertirse en uno de los mejores Ninja del lugar");
+    }
+}
