28fc63d786660e02c3be476a35f4faa38b5d9920
Ricardo Buendia <ricardobuendia@ciencias.unam.mx>
2025-11-13 22:32:48 -0600

    feat(state): se agrega el patron state

---
+++ proyecto02/Proyecto02_espora/src/main/java/mx/unam/fciencias/espora/proyecto2/EstadoCritico.java
@@ -0,0 +1,44 @@
+import mx.unam.fciencias.espora.proyecto2;
+
+/**
+ * Esta clase representa el estado critico de una poblacion,
+ * implementando los metodos de la interfaz EstadoPoblacion.
+ * 
+ * @author Equipo Espora
+ * @version 1.0
+ */
+
+public class EstadoCritico implements EstadoPoblacion{
+
+    private PoblacionEspecie poblacion;
+
+    /**
+     * Constructor de la clase
+     * @param poblacion la poblacion del estado
+     */
+    public EstadoRiesgo(PoblacionEspecie poblacion) {
+        this.poblacion = poblacion;
+    }
+    
+    /**
+     * Metodos implementados de la interfaz EstadoPoblacion
+     */
+    @Override
+    public void reproducir() {
+        int actual = poblacion.getTamanio();
+        int nuevos = (int)(actual * 0.20);
+        System.out.println("Estado Critico: Nacen " + nuevos + " nuevas especies");
+        poblacion.setTamanio(actual + nuevos);
+    }
+
+    /**
+     * Metodo de morir de forma natural
+     */
+    @Override
+    public void morirNatural() {
+        int actual = poblacion.getTamanio();
+        int mueren = (int)(actual * 0.05);
+        System.out.println("Estado Critico: Mueren " + mueren + " especies por causas naturales");
+        poblacion.setTamanio(actual - mueren);
+    }
+}
