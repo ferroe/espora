5e3c4f5e36fa63fb10caef63eedc3310d77ce332
Ricardo Buendia <ricardobuendia@ciencias.unam.mx>
2025-11-15 18:38:36 -0600

    feat(strategy): Se agrega Strategy y PoblacionEspecie

---
+++ proyecto02/Proyecto02_espora/src/main/java/mx/unam/fciencias/espora/proyecto2/EfectoEstrategia.java
@@ -0,0 +1,18 @@
+import mx.unam.fciencias.espora.proyecto2;
+import java.util.Map;
+
+/**
+ * Esta interfaz es la estrategia de efecto
+ * 
+ * @author Equipo Espora
+ * @version 1.0
+ */
+public interface EfectoEstrategia {
+
+    /**
+     * Calcula el efecto de la estrategia en la poblacion
+     * @param parametros Los parametros ambientales que afectan a la poblacion
+     */
+    public void calcularEfecto(PoblacionEspecie poblacion, Map<String, Double> parametros);
+    
+}
---
+++ proyecto02/Proyecto02_espora/src/main/java/mx/unam/fciencias/espora/proyecto2/EstrategiaRanaMoctezuma.java
@@ -0,0 +1,36 @@
+import mx.unam.fciencias.espora.proyecto2;
+import java.util.Map;
+
+/**
+ * Esta es la estrategia para la rana moctezuma, que implementa la
+ * clase EfectoEstrategia
+ * 
+ * @author Equipo Espora
+ * @version 1.0
+ */
+public class EstrategiaRanaMoctezuma implements EfectoEstrategia {
+
+    /**
+     * Metodo que calcula el efecto de la estrategia de la poblacion
+     * en el que la rana moctezuma reacciona con la contaminacion y especies invasoras
+     * 
+     * @param poblacion La poblacion a la que se le aplica el efecto
+     * @param parametros los parametros ambientales que afectan a la poblacion
+     */
+    @Override
+    public void calcularEfecto(PoblacionEspecie poblacion, Map<String, Double> parametros) {
+        System.out.println("La Rana Moctezuma: " + poblacion.getNombre());
+
+        double contaminacion = parametros.getOrDefault("contaminacion", 0.0);
+        double invasoras = parametros.getOrDefault("especiesInvasoras", 0.0);
+        if (contaminacion > 0.4 && invasoras > 0.4) {
+            System.out.println("Hay un amenaza combinada (Contaminación: " + contaminacion + ", Invasoras: " + invasoras + ")");
+            double saludActual = poblacion.getSalud();
+            poblacion.setSaludPromedio(saludActual * 0.90);
+            System.out.println("Su nueva salud es: " + poblacion.getSalud());
+
+        } else {
+            System.out.println("Estas en condiciones aceptables, la rana resiste.");
+        }
+    }
+}
